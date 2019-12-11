37d62537fe20a1b132f1813c65b411dacdcb8bb9
Paul Riseborough <p_riseborough@live.com.au>
2019-12-11 15:33:16 +1100

    logger: add test data for emergency yaw estimator

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -550,6 +550,7 @@ void Logger::add_default_topics()
 	add_topic("vehicle_status", 200);
 	add_topic("vehicle_status_flags");
 	add_topic("vtol_vehicle_status", 200);
+	add_topic("yaw_est_test_data");
 
 	add_topic_multi("actuator_outputs", 100);
 	add_topic_multi("battery_status", 500);

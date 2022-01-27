557e2bf5cb771f667c7317a8bd8d3a54c4daeb92
bresch <brescianimathieu@gmail.com>
2022-01-27 17:08:02 +0100

    update terrain validity all the time

--- src/modules/ekf2/EKF/control.cpp
+++ src/modules/ekf2/EKF/control.cpp
@@ -936,6 +936,7 @@ void Ekf::controlHeightFusion()
 	if ((use_for_hagl || _control_status.flags.rng_hgt) && _range_sensor.isDataHealthy()) {
 		fuseHaglAllStates();
 	}
+	updateTerrainValidity();
 }
 
 void Ekf::checkRangeAidSuitability()
